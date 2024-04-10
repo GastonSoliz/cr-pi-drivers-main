b10eddbfa34394a4b6f71769a6ded0b454c64fa1
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-04-10 19:26:07 -0300

    migrating actions from javascript to typescript

--- cr-pi-drivers-main/client/src/redux/actions.ts
+++ cr-pi-drivers-main/client/src/redux/actions.ts
@@ -1,27 +1,45 @@
 import axios from "axios";
+import { Dispatch } from "redux";
 
-const URL = "http://localhost:3001/";
+type Team = {
+  id: number;
+  name: string;
+};
+
+type Driver = {
+  id?: number;
+  birthdate: string;
+  description: string;
+  image: string;
+  name: string;
+  nationality: string;
+  surname: string;
+  //DECIDIR SI AL FINAL TEAM VIENE COMO STRING O ARRAY
+  teams: string;
+};
+
+const URL: string = "http://localhost:3001/";
 
 export function getTeams() {
-  const endpoint = `${URL}teams`;
-  return async (dispatch) => {
-    const { data } = await axios.get(endpoint);
+  const endpoint: string = `${URL}teams`;
+  return async (dispatch: Dispatch) => {
+    const { data } = await axios.get<Team[]>(endpoint);
     return dispatch({ type: "GET_TEAMS", payload: data });
   };
 }
 
-export function getDriverByName(name) {
-  const endpoint = `${URL}drivers?name=${name}`;
-  return async (dispatch) => {
-    const { data } = await axios.get(endpoint);
+export function getDriverByName(name: string) {
+  const endpoint: string = `${URL}drivers?name=${name}`;
+  return async (dispatch: Dispatch) => {
+    const { data } = await axios.get<Driver[] | Driver>(endpoint);
     return dispatch({ type: "GET_DRIVER_BY_NAME", payload: data });
   };
 }
 
-export function getDriverById(id) {
-  const endpoint = `${URL}drivers/${id}`;
-  return async (dispatch) => {
-    const { data } = await axios.get(endpoint);
+export function getDriverById(id: number) {
+  const endpoint: string = `${URL}drivers/${id}`;
+  return async (dispatch: Dispatch) => {
+    const { data } = await axios.get<Driver>(endpoint);
     if (data) {
       return dispatch({ type: "GET_DRIVER_BY_ID", payload: data });
     }
@@ -29,50 +47,52 @@ export function getDriverById(id) {
 }
 
 export function getDrivers() {
-  const endpoint = `${URL}drivers`;
-  return async (dispatch) => {
-    const { data } = await axios.get(endpoint);
+  const endpoint: string = `${URL}drivers`;
+  return async (dispatch: Dispatch) => {
+    const { data } = await axios.get<Driver[]>(endpoint);
     return dispatch({ type: "GET_DRIVERS", payload: data });
   };
 }
 
-export function postDriver(driver) {
-  const endpoint = `${URL}drivers`;
-  return async (dispatch) => {
-    const { data } = await axios.post(endpoint, driver);
+export function postDriver(driver: Driver) {
+  const endpoint: string = `${URL}drivers`;
+  return async (dispatch: Dispatch) => {
+    const { data } = await axios.post<Driver>(endpoint, driver);
     return dispatch({ type: "POST_DRIVER", payload: data });
   };
 }
 
-export function deleteDriver(id) {
-  const endpoint = `${URL}drivers/${id}`;
-  return async (dispatch) => {
-    const { data } = await axios.delete(endpoint);
+//FIJARSE SI FUNCONA ESTE ENDPOINT
+export function deleteDriver(id: number) {
+  const endpoint: string = `${URL}drivers/${id}`;
+  return async (dispatch: Dispatch) => {
+    await axios.delete<Driver>(endpoint);
     return dispatch({ type: "DELETE_DRIVER", payload: id });
   };
 }
 
-export function updateDriver(driver, id) {
-  const endpoint = `${URL}drivers/${id}`;
-  return async (dispatch) => {
+//QUE DEBERIA PONER EN EL GENERCO DEL AXIOS.PUT?
+export function updateDriver(driver: Driver, id: number) {
+  const endpoint: string = `${URL}drivers/${id}`;
+  return async (dispatch: Dispatch) => {
     const { data } = await axios.put(endpoint, driver);
     return dispatch({ type: "UPDATE_DRIVER", payload: data });
   };
 }
 
-export function sortOrigin(origin_id) {
+export function sortOrigin(origin_id: string) {
   return { type: "SORT_ORIGIN", payload: origin_id };
 }
 
-export function sortDate(date) {
+export function sortDate(date: string) {
   return { type: "SORT_DATE", payload: date };
 }
 
-export function sortName(name) {
+export function sortName(name: string) {
   return { type: "SORT_NAME", payload: name };
 }
 
-export function sortTeam(team) {
+export function sortTeam(team: string) {
   return { type: "SORT_TEAM", payload: team };
 }
 
