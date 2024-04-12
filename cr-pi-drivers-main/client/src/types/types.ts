14a85069a4d1ba2386e1c3d134a6a88b268eb4e4
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-04-11 23:12:24 -0300

    creating a types folder for modulation

---
+++ cr-pi-drivers-main/client/src/types/types.ts
@@ -0,0 +1,31 @@
+export type Team = {
+  id: number;
+  name: string;
+};
+
+export type Driver = {
+  birthdate: string;
+  description: string;
+  image: string;
+  name: string;
+  nationality: string;
+  surname: string;
+  teams: Team[];
+};
+
+export type DriverError = {
+  birthdate?: string;
+  description?: string;
+  image?: string;
+  name?: string;
+  nationality?: string;
+  surname?: string;
+  teams?: string;
+};
+
+export type State = {
+  allDrivers: Driver[];
+  filteredDrivers: Driver[] | Driver;
+  driverDetail: Driver | null;
+  allTeams: Team[];
+};
