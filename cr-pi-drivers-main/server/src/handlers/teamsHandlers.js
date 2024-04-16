079cfed8479fdd73facd4a94fdeeb7a6f60cd86d
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-04-16 17:07:35 -0300

    deleting js files

--- cr-pi-drivers-main/server/src/handlers/teamsHandlers.js
+++
@@ -1,8 +0,0 @@
-const { getAllTeams } = require("../controllers/teamControllers");
-
-const getTeams = async (req, res) => {
-  const results = await getAllTeams();
-  res.status(200).json(results);
-};
-
-module.exports = getTeams;
