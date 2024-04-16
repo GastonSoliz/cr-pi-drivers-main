171398b8e9690562ca67749419873d3a0c5ec6c8
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-04-15 22:50:38 -0300

    new style to navbar

--- cr-pi-drivers-main/client/src/components/NavBar/NavBar.tsx
+++ cr-pi-drivers-main/client/src/components/NavBar/NavBar.tsx
@@ -1,20 +1,26 @@
 import { Link } from "react-router-dom";
-import SearchBar from "../SearchBar/SearchBar";
-import style from "./NavBar.module.css";
+//import style from "./NavBar.module.css";
 import React from "react";
 
 export default function NavBar() {
   return (
-    <div className={style.NavBarContainer}>
-      <div className={style.buttonsContainer}>
-        <Link to="/home">
-          <button>HOME</button>
-        </Link>
-        <Link to="/form">
-          <button>FORM</button>
-        </Link>
+    <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+      <div className="container-fluid">
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link to="/home" className="nav-link">
+                HOME
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/form" className="nav-link">
+                FORM
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <SearchBar />
-    </div>
+    </nav>
   );
 }
