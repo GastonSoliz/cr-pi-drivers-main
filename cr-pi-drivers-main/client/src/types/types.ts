f8eec0ade40a81c35b346c2ba8a3fde99e6d9b0e
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-04-17 13:30:58 -0300

    modifying state type

--- cr-pi-drivers-main/client/src/types/types.ts
+++ cr-pi-drivers-main/client/src/types/types.ts
@@ -30,4 +30,25 @@ export type State = {
   filteredDrivers: Driver[] | Driver;
   driverDetail: Driver | null;
   allTeams: Team[];
+  msj: string | null;
 };
+
+export type Action =
+  | { type: "GET_TEAMS"; payload: Team[] }
+  //[{Driver}]
+  | { type: "GET_DRIVER_BY_NAME"; payload: Driver[] | Driver }
+  | { type: "GET_DRIVER_BY_ID"; payload: Driver }
+  | { type: "GET_DRIVERS"; payload: Driver[] }
+  //{newDriver:{Driver}} deberia ir todo de la misma manera... sino lo hace... ARREGLAR TODO EL PROYECTO
+  //Si no, es quilombo de tipo de datos
+  | { type: "POST_DRIVER_REQUEST"; payload: string }
+  | { type: "POST_DRIVER_SUCCESS"; payload: Driver }
+  | { type: "POST_DRIVER_FAILURE"; payload: string }
+  | { type: "DELETE_DRIVER"; payload: number }
+  | { type: "UPDATE_DRIVER"; payload: Driver }
+  | { type: "SORT_ORIGIN"; payload: string }
+  | { type: "SORT_DATE"; payload: string }
+  //Falta implementar
+  | { type: "SORT_NAME"; payload: string }
+  | { type: "SORT_TEAM"; payload: string }
+  | { type: "CLEAN_DETAIL"; payload: null };
