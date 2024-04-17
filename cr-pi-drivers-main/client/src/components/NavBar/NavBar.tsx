e780cdf2495e07ec10955d0670ca214b4a6c5d8f
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-04-17 14:06:42 -0300

    adding responsive navbar

--- cr-pi-drivers-main/client/src/components/NavBar/NavBar.tsx
+++ cr-pi-drivers-main/client/src/components/NavBar/NavBar.tsx
@@ -1,21 +1,29 @@
 import { Link } from "react-router-dom";
 //import style from "./NavBar.module.css";
-import React from "react";
+import React, { useState } from "react";
 
 export default function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-primary">
       <div className="container-fluid">
-        <div className="collapse navbar-collapse">
+        <button className="navbar-toggler" type="button" onClick={toggleNavbar}>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}>
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link to="/home" className="nav-link">
-                HOME
+              <Link to="/home" className="nav-link text-light">
+                Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/form" className="nav-link">
-                FORM
+              <Link to="/form" className="nav-link text-light">
+                Create Driver
               </Link>
             </li>
           </ul>
