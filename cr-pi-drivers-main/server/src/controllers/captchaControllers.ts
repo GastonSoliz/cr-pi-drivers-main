b2d13ffba06be5a1127b9e0a5f05e123802306f4
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-04-26 21:29:53 -0300

    passing secret key to .env file

--- cr-pi-drivers-main/server/src/controllers/captchaControllers.ts
+++ cr-pi-drivers-main/server/src/controllers/captchaControllers.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
+const { SECRET_KEY } = process.env;
 
 export const validateCaptcha = async (token: string) => {
-  const secretKey = "6Le368MpAAAAAOaq294Kit3X4Rtw3ogrDAnuK0sE";
-  const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`;
+  const url = `https://www.google.com/recaptcha/api/siteverify?secret=${SECRET_KEY}&response=${token}`;
 
   const { data } = await axios.post(url);
   return data;
