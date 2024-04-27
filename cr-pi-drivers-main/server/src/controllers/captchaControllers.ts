5ed66f19502fcb00a597cf4372c3bb330e961bf1
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-04-26 23:13:53 -0300

    adding type to function

--- cr-pi-drivers-main/server/src/controllers/captchaControllers.ts
+++ cr-pi-drivers-main/server/src/controllers/captchaControllers.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
+import { CaptchaRequest } from "../types/types";
 const { SECRET_KEY } = process.env;
 
-export const validateCaptcha = async (token: string) => {
+export const validateCaptcha = async (
+  token: string
+): Promise<CaptchaRequest> => {
   const url = `https://www.google.com/recaptcha/api/siteverify?secret=${SECRET_KEY}&response=${token}`;
 
   const { data } = await axios.post(url);
