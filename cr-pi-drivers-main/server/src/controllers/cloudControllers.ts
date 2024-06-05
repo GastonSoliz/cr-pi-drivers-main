3b2551ced44f1c3873f60aba95515eca39076c91
Gaston Soliz <gaston.e.soliz@gmail.com>
2024-06-05 00:43:19 -0300

    fixing cloud functions

--- cr-pi-drivers-main/server/src/controllers/cloudControllers.ts
+++ cr-pi-drivers-main/server/src/controllers/cloudControllers.ts
@@ -1,5 +1,7 @@
 const { CLOUD_NAME, CLOUD_KEY, CLOUD_SECRET } = process.env;
 const cloudinary = require("cloudinary").v2;
+const streamifier = require ("streamifier");
+
 
 cloudinary.config({
   cloud_name: CLOUD_NAME,
@@ -7,10 +9,40 @@ cloudinary.config({
   api_secret: CLOUD_SECRET,
 });
 
-export const cloudController = async (img: string) => {
-  console.log("cloud controller: ", img);
-  const cloudInfo = await cloudinary.uploader.upload(img, {
-    folder: "uploads",
+export const cloudController = async (img: Buffer) => {
+  try{
+    // const imageStream = streamifier.createReadStream(img);
+    // console.log("cloud controller: ", imageStream);
+    // const cloudInfo = await cloudinary.uploader.upload(imageStream, {
+    //   folder: "uploads",
+    // });
+    //console.log("cloud controller: ", img);
+    // const cloudInfo = await cloudinary.uploader.upload_stream(
+    //   {
+    //     folder: "drivers"
+    //   },
+    //   function(error : any, result : any) {
+    //       console.log("toca esto?",error, result);
+    //   }
+    // );
+    return new Promise((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        {
+          folder: "drivers"
+        },
+        function(error: any, result: any) {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(result);
+          }
+        }
+      );
+    streamifier.createReadStream(img).pipe(uploadStream);
   });
-  return cloudInfo;
+    // return cloudInfo;
+  }catch(error){
+    console.log(error);
+    throw error;
+  }
 };
